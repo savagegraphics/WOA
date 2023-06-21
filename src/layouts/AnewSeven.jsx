c9c329dfb1a64286875bd1cf2e763181082c4b95
savagegraphics <olawaleraji188@gmail.com>
2023-06-20 20:51:55 -0700

    first commit

--- src/layouts/AnewSeven.jsx
+++ src/layouts/AnewSeven.jsx
@@ -13,47 +13,51 @@ const AnewSeven = () => {
                     </div>
                     <div className="max-w-xl mb-10">
                         <h2 className="text-3xl font-extrabold text-[#85586F] dark:text-white sm:text-4xl">
-                            <span className="block">
-                                FREE Hello Baby Box
-                            </span>
+                            <span className="block">FREE Hello Baby Box</span>
                         </h2>
                         <p className="text-base text-gray-700 md:text-lg mt-10">
-                            Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-                            accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
-                            quae. explicabo.
+                            Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque
+                            laudantium, totam rem aperiam, eaque ipsa quae. explicabo.
                         </p>
                     </div>
                     <div>
-                        <button type="button" style={ { backgroundColor: '#85586F' } } className="py-2 px-4 hover:bg-white focus:ring-D14D72-500 focus:ring-offset-D14D72-200 text-white w-sm transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg ">
+                        <button
+                            type="button"
+                            style={ { backgroundColor: '#85586F' } }
+                            className="py-2 px-4 hover:bg-white focus:ring-D14D72-500 focus:ring-offset-D14D72-200 text-white w-sm transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2 rounded-lg"
+                        >
                             Get started
                         </button>
                     </div>
                 </div>
                 <div className="flex items-center justify-center -mx-4 lg:pl-8">
                     <div className="flex flex-col items-end px-3">
-                        <Image
-                            className="object-cover mb-6 rounded shadow-lg h-28 sm:h-48 xl:h-56 w-28 sm:w-48 xl:w-56"
-                            src="https://images.pexels.com/photos/3184287/pexels-photo-3184287.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=2&amp;h=750&amp;w=1260"
-                            alt=""
-                            width={ 1260 }
-                            height={ 750 }
-                        />
-                        <Image
-                            className="object-cover w-20 h-20 rounded shadow-lg sm:h-32 xl:h-40 sm:w-32 xl:w-40"
-                            src="https://images.pexels.com/photos/3182812/pexels-photo-3182812.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=2&amp;h=750&amp;w=1260"
-                            alt=""
-                            width={ 1260 }
-                            height={ 750 }
-                        />
+                        <div className="relative w-28 h-28 sm:w-48 sm:h-48 xl:w-56 xl:h-56">
+                            <Image
+                                className="object-cover rounded shadow-lg"
+                                src="https://images.pexels.com/photos/3184287/pexels-photo-3184287.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=2&amp;h=750&amp;w=1260"
+                                alt=""
+                                layout="fill"
+                            />
+                        </div>
+                        <div className="relative w-20 h-20 sm:w-32 sm:h-32 xl:w-40 xl:h-40 mt-6">
+                            <Image
+                                className="object-cover rounded shadow-lg"
+                                src="https://images.pexels.com/photos/3182812/pexels-photo-3182812.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=2&amp;h=750&amp;w=1260"
+                                alt=""
+                                layout="fill"
+                            />
+                        </div>
                     </div>
                     <div className="px-3">
-                        <Image
-                            className="object-cover w-40 h-40 rounded shadow-lg sm:h-64 xl:h-80 sm:w-64 xl:w-80"
-                            src="https://images.pexels.com/photos/3182739/pexels-photo-3182739.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=2&amp;w=500"
-                            alt=""
-                            width={ 1260 }
-                            height={ 750 }
-                        />
+                        <div className="relative w-40 h-40 sm:w-64 sm:h-64 xl:w-80 xl:h-80">
+                            <Image
+                                className="object-cover rounded shadow-lg"
+                                src="https://images.pexels.com/photos/3182739/pexels-photo-3182739.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=2&amp;w=500"
+                                alt=""
+                                layout="fill"
+                            />
+                        </div>
                     </div>
                 </div>
             </div>
@@ -61,4 +65,4 @@ const AnewSeven = () => {
     );
 };
 
-export default AnewSeven
+export default AnewSeven;

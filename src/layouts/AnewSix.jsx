54363c6c27091e0c0414517a587ca4b65980d1e0
savagegraphics <olawaleraji188@gmail.com>
2023-06-21 20:39:06 -0700

    first commit

--- src/layouts/AnewSix.jsx
+++ src/layouts/AnewSix.jsx
@@ -5,13 +5,17 @@ const AnewSix = () => {
     return (
         <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
             <div className="grid max-w-screen-lg gap-8 row-gap-5 md:row-gap-8 sm:mx-auto lg:grid-cols-2">
-                <div style={ { backgroundColor: '#fff' } } className="transition duration-300 transform bg-white rounded shadow-sm hover:-translate-y-1 hover:shadow md:text-center">
+                <div
+                    style={ { backgroundColor: '#fff' } }
+                    className="transition duration-300 transform bg-white rounded shadow-sm hover:-translate-y-1 hover:shadow md:text-center"
+                >
                     <div className="relative h-64 lg:h-80 xl:h-96">
                         <Image
                             className="object-cover w-full h-full rounded-t"
                             src="https://images.pexels.com/photos/3182796/pexels-photo-3182796.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=2&amp;h=750&amp;w=1260"
                             alt=""
-                            layout="fill"
+                            width={ 1260 }
+                            height={ 750 }
                         />
                         <div className="absolute inset-0 bg-gray-800 bg-opacity-25" />
                     </div>
@@ -27,7 +31,7 @@ const AnewSix = () => {
                         <button
                             type="submit"
                             className="inline-flex items-center justify-center h-12 px-6 font-medium tracking-wide text-white transition duration-200 rounded shadow-md bg-deep-purple-accent-400 hover:bg-deep-purple-accent-700 focus:shadow-outline focus:outline-none"
-                            style={ { backgroundColor: "deeppink" } }
+                            style={ { backgroundColor: 'deeppink' } }
                         >
                             Read more
                         </button>
@@ -39,7 +43,8 @@ const AnewSix = () => {
                             className="object-cover w-full h-full rounded-t"
                             src="https://images.pexels.com/photos/3184360/pexels-photo-3184360.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=2&amp;h=750&amp;w=1260"
                             alt=""
-                            layout="fill"
+                            width={ 1260 }
+                            height={ 750 }
                         />
                         <div className="absolute inset-0 bg-gray-800 bg-opacity-25" />
                     </div>
@@ -55,7 +60,7 @@ const AnewSix = () => {
                         <button
                             type="submit"
                             className="inline-flex items-center justify-center h-12 px-6 font-medium tracking-wide text-white transition duration-200 rounded shadow-md bg-deep-purple-accent-400 hover:bg-deep-purple-accent-700 focus:shadow-outline focus:outline-none"
-                            style={ { backgroundColor: "deeppink" } }
+                            style={ { backgroundColor: 'deeppink' } }
                         >
                             Read more
                         </button>

6a186be979f95f0001b8e8dc1a0f338977f61638
savagegraphics <olawaleraji188@gmail.com>
2023-06-22 11:59:00 -0700

    first commit

--- src/components/Landpage.jsx
+++ src/components/Landpage.jsx
@@ -7,7 +7,7 @@ const Landpage = () => {
             <div className="grid grid-cols-1 gap-12 md:grid-cols-2 xl:grid-cols-2">
                 <div className="m-auto overflow-hidden rounded-lg shadow-lg cursor-pointer h-90 w-60 md:w-80">
                     <a href="#" className="block w-full h-full">
-                        <div className="w-full p-4 bg-white dark:bg-gray-800">
+                        <div style={ { backgroundColor: '#fff' } } className="w-full p-4 bg-white dark:bg-gray-800">
                             <BsFilePlus className="ml-16 lg:ml-28 mt-8 mb-8" size={ 44 } color='#85586F' />
                             <p className="mb-2 text-xl pl-4 pr-4 font-bold text-[#85586F] dark:text-white">
                                 Create a Babylist
@@ -23,7 +23,7 @@ const Landpage = () => {
                 </div>
                 <div className="m-auto overflow-hidden rounded-lg shadow-lg cursor-pointer h-90 w-60 md:w-80">
                     <a href="#" className="block w-full h-full">
-                        <div className="w-full p-4 bg-white dark:bg-gray-800">
+                        <div style={ { backgroundColor: '#fff' } } className="w-full p-4 bg-white dark:bg-gray-800">
                             <BsSearch className="ml-16 lg:ml-28 mt-8 mb-8" size={ 44 } color='#85586F' />
                             <p className="mb-2 text-xl pl-4 pr-4 font-bold text-[#85586F] dark:text-white">
                                 Find a Babylist

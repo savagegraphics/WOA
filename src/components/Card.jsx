6e6fa5a20211755b354c3a84fc6bdd9ff3c6991a
savagegraphics <olawaleraji188@gmail.com>
2023-06-21 20:20:39 -0700

    first commit

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -9,7 +9,7 @@ const Card = () => {
     return (
         <div className="flex-wrap items-center justify-center gap-8 mt-12 mb-12 text-left sm:flex">
             <div style={ { background: '#fff' } } className="w-full sm:w-1/2 md:w-1/2 lg:w-1/4 p-8 pl-12 mt-6 rounded-lg shadow-lg dark:bg-gray-800">
-                <p className="text-gray-500 text-md dark:text-gray-300">
+                <p className="text-[#6a1a447d] text-md dark:text-gray-300">
                     <span className="text-lg font-bold text-custom-D14D72">
                         “
                     </span>
@@ -26,14 +26,14 @@ const Card = () => {
                         <span className="text-sm font-semibold text-[#85586F]">
                             Monet T.
                         </span>
-                        <span className="flex items-center text-gray-500 text-xs dark:text-gray-300">
+                        <span className="flex items-center text-[#6a1a447d] text-xs dark:text-gray-300">
                             User of Babylist
                         </span>
                     </div>
                 </div>
             </div>
             <div style={ { background: '#fff' } } className="w-full sm:w-1/2 md:w-1/2 lg:w-1/4 sm:mt-16 md:mt-20 lg:mt-24 p-8 pl-12 mt-6 rounded-lg shadow-lg dark:bg-gray-800">
-                <p className="text-gray-500 text-md dark:text-gray-300">
+                <p className="text-[#6a1a447d] text-md dark:text-gray-300">
                     <span className="text-lg font-bold text-custom-D14D72">
                         “
                     </span>
@@ -50,14 +50,14 @@ const Card = () => {
                         <span className="text-sm font-semibold text-[#85586F]">
                             Omolara M.
                         </span>
-                        <span className="flex items-center text-gray-500 text-xs dark:text-gray-300">
+                        <span className="flex items-center text-[#6a1a447d] text-xs dark:text-gray-300">
                             User of Babylist
                         </span>
                     </div>
                 </div>
             </div>
             <div style={ { background: '#fff' } } className="w-full sm:w-1/2 md:w-1/2 lg:w-1/4 p-8 pl-12 mt-6 rounded-lg shadow-lg dark:bg-gray-800">
-                <p className="text-gray-500 text-md dark:text-gray-300">
+                <p className="text-[#6a1a447d] text-md dark:text-gray-300">
                     <span className="text-lg font-bold text-custom-D14D72">
                         “
                     </span>
@@ -74,7 +74,7 @@ const Card = () => {
                         <span className="text-sm font-semibold text-[#85586F]">
                             Doja C.
                         </span>
-                        <span className="flex items-center text-gray-500 text-xs dark:text-gray-300">
+                        <span className="flex items-center text-[#6a1a447d] text-xs dark:text-gray-300">
                             User of Babylist
                         </span>
                     </div>

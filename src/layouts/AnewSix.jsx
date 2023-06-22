507e0c01d86bbfcea3c3543e85e0fa1b8486b623
savagegraphics <olawaleraji188@gmail.com>
2023-06-21 20:03:03 -0700

    first commit

--- src/layouts/AnewSix.jsx
+++ src/layouts/AnewSix.jsx
@@ -5,7 +5,7 @@ const AnewSix = () => {
     return (
         <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
             <div className="grid max-w-screen-lg gap-8 row-gap-5 md:row-gap-8 sm:mx-auto lg:grid-cols-2">
-                <div className="transition duration-300 transform bg-white rounded shadow-sm hover:-translate-y-1 hover:shadow md:text-center">
+                <div style={ { backgroundColor: '#fff' } } className="transition duration-300 transform rounded shadow-sm hover:-translate-y-1 hover:shadow md:text-center">
                     <div className="relative">
                         <Image
                             className="object-cover w-full h-64 rounded-t lg:h-80 xl:h-96"

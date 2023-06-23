99cd80e66c8961d9ceb51c5ecffd967c82202b49
savagegraphics <olawaleraji188@gmail.com>
2023-06-22 21:14:26 -0700

    first commit

--- src/components/Landpage.jsx
+++ src/components/Landpage.jsx
@@ -23,12 +23,12 @@ const Landpage = () => {
                 </div>
                 <div className="m-auto overflow-hidden rounded-lg shadow-lg cursor-pointer h-90 w-60 md:w-80">
                     <a href="#" className="block w-full h-full">
-                        <div style={ { backgroundColor: '#fff' } } className="w-full p-4 bg-white dark:bg-gray-800">
+                        <div className="w-full p-4 bg-white dark:bg-gray-800">
                             <BsSearch className="ml-16 lg:ml-28 mt-8 mb-8" size={ 44 } color='#85586F' />
-                            <p style={ { color: '#85586F' } } className="mb-2 text-xl pl-4 pr-4 font-bold text-[#85586F] dark:text-white">
+                            <p className="mb-2 text-xl pl-4 pr-4 font-bold text-[#85586F] dark:text-white">
                                 Find a Babylist
                             </p>
-                            <p style={ { color: '#606C5D' } } className="font-light pl-4 pr-3 lg:pl-2 lg:pr-0 text-gray-400 dark:text-gray-300 text-md">
+                            <p className="font-light pl-4 pr-3 lg:pl-2 lg:pr-0 text-gray-400 dark:text-gray-300 text-md">
                                 Give  the perfect gift to the new family.
                             </p>
                             <button type="button" style={ { color: '#85586F', } } className="py-2 px-4  mt-4 mb-4 bg-custom-D14D72 hover:bg-D14D72-700 focus:ring-D14D72-500 focus:ring-offset-D14D72-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg ">

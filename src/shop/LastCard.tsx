bb6ec50de972644b2641d633795ebe89d4a78e7d
savagegraphics <olawaleraji188@gmail.com>
2023-07-13 19:04:00 -0700

    first commit

--- src/shop/LastCard.tsx
+++ src/shop/LastCard.tsx
@@ -1,54 +1,128 @@
-import React from 'react'
+import React from 'react';
 import './LastCard.css'
-
-
-
-const products = [
-    {
-        id: 1,
-        href: '#',
-        imageSrc: 'https://images.unsplash.com/photo-1519689680058-324335c77eba?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8YmFieSUyMHByb2R1Y3RzfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60',
-        title: 'Kelsie Meyer',
-    },
-    {
-        id: 2,
-        href: '#',
-        imageSrc: 'https://images.unsplash.com/photo-1519689680058-324335c77eba?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8YmFieSUyMHByb2R1Y3RzfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60',
-        title: 'Mark Carusso',
-    },
-    {
-        id: 3,
-        href: '#',
-        imageSrc: 'https://images.unsplash.com/photo-1519689680058-324335c77eba?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8YmFieSUyMHByb2R1Y3RzfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60',
-        title: 'Taylor Green',
-    },
-    {
-        id: 4,
-        href: '#',
-        imageSrc: 'https://images.unsplash.com/photo-1519689680058-324335c77eba?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8YmFieSUyMHByb2R1Y3RzfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60',
-        title: 'Isaiah Jian',
-    },
-]
 
 const LastCard = () => {
   return (
-    <div className='root'>
-  <div className="wrap custom-grid-cols-2 ">
-  {products.map((product) => (
-  <div key={product.id} className="box">
-    <div className="box-top">
-      <img className="box-image" 
-        src={product.imageSrc}
-    />
-      <div className="title-flex">
-        <h3 className="box-title">{product.title}</h3>
-      </div>
+    <div className="grid md:grid-cols-3 gap-4 sm:grid-cols-2 custom-grid-cols-2">
+      <div className="max-w-xs rounded-md shadow-md dark:bg-gray-900 dark:text-gray-100">
+        <img
+          src="https://source.unsplash.com/random/300x300/?2"
+          alt=""
+          className="object-cover object-center w-full rounded-t-md h-72 dark:bg-gray-500"
+        />
+        <div className="flex flex-col justify-between p-6 space-y-8">
+          <button
+            type="button"
+            className="flex items-center justify-center w-full p-3 font-semibold tracking-wide rounded-md dark:bg-violet-400 dark:text-gray-900"
+          >
+            Read more
+          </button>
         </div>
-  </div> 
-  ) )}
-</div>
+      </div>
+      <div className="max-w-xs rounded-md shadow-md dark:bg-gray-900 dark:text-gray-100">
+        <img
+          src="https://source.unsplash.com/random/300x300/?2"
+          alt=""
+          className="object-cover object-center w-full rounded-t-md h-72 dark:bg-gray-500"
+        />
+        <div className="flex flex-col justify-between p-6 space-y-8">
+          <button
+            type="button"
+            className="flex items-center justify-center w-full p-3 font-semibold tracking-wide rounded-md dark:bg-violet-400 dark:text-gray-900"
+          >
+            Read more
+          </button>
+        </div>
+      </div>
+      <div className="max-w-xs rounded-md shadow-md dark:bg-gray-900 dark:text-gray-100">
+        <img
+          src="https://source.unsplash.com/random/300x300/?2"
+          alt=""
+          className="object-cover object-center w-full rounded-t-md h-72 dark:bg-gray-500"
+        />
+        <div className="flex flex-col justify-between p-6 space-y-8">
+          <button
+            type="button"
+            className="flex items-center justify-center w-full p-3 font-semibold tracking-wide rounded-md dark:bg-violet-400 dark:text-gray-900"
+          >
+            Read more
+          </button>
+        </div>
+      </div>
+      <div className="max-w-xs rounded-md shadow-md dark:bg-gray-900 dark:text-gray-100">
+        <img
+          src="https://source.unsplash.com/random/300x300/?2"
+          alt=""
+          className="object-cover object-center w-full rounded-t-md h-72 dark:bg-gray-500"
+        />
+        <div className="flex flex-col justify-between p-6 space-y-8">
+          <button
+            type="button"
+            className="flex items-center justify-center w-full p-3 font-semibold tracking-wide rounded-md dark:bg-violet-400 dark:text-gray-900"
+          >
+            Read more
+          </button>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default LastCard
+export default LastCard;
+
+
+
+// import React from 'react'
+// import './LastCard.css'
+
+
+
+// const products = [
+//     {
+//         id: 1,
+//         href: '#',
+//         imageSrc: 'https://images.unsplash.com/photo-1519689680058-324335c77eba?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8YmFieSUyMHByb2R1Y3RzfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60',
+//         title: 'Kelsie Meyer',
+//     },
+//     {
+//         id: 2,
+//         href: '#',
+//         imageSrc: 'https://images.unsplash.com/photo-1519689680058-324335c77eba?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8YmFieSUyMHByb2R1Y3RzfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60',
+//         title: 'Mark Carusso',
+//     },
+//     {
+//         id: 3,
+//         href: '#',
+//         imageSrc: 'https://images.unsplash.com/photo-1519689680058-324335c77eba?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8YmFieSUyMHByb2R1Y3RzfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60',
+//         title: 'Taylor Green',
+//     },
+//     {
+//         id: 4,
+//         href: '#',
+//         imageSrc: 'https://images.unsplash.com/photo-1519689680058-324335c77eba?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8YmFieSUyMHByb2R1Y3RzfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60',
+//         title: 'Isaiah Jian',
+//     },
+// ]
+
+// const LastCard = () => {
+//   return (
+//     <div className='root'>
+//   <div className="wrap custom-grid-cols-2 ">
+//   {products.map((product) => (
+//   <div key={product.id} className="box">
+//     <div className="box-top">
+//       <img className="box-image" 
+//         src={product.imageSrc}
+//     />
+//       <div className="title-flex">
+//         <h3 className="box-title">{product.title}</h3>
+//       </div>
+//         </div>
+//   </div> 
+//   ) )}
+// </div>
+//     </div>
+//   )
+// }
+
+// export default LastCard
